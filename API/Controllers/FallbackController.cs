d9369554a28183343931a2e2f32a1fefb4edcce8
Hamzagunay41 <56889887+Hamzagunay41@users.noreply.github.com>
2024-01-04 13:27:02 +0300

    test2222

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -1,16 +1,14 @@
-
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
-{       
-    [AllowAnonymous]
-       public class FallbackController : Controller
+namespace API.Controllers;
+
+[AllowAnonymous]
+public class FallbackController : Controller
+{
+    public IActionResult Index()
     {
-        public IActionResult Index ()
-        {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
-            "wwwroot","index.html"), "text/HTML");
-        }
+        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"),
+            "text/HTML");
     }
 }
